b135c62a0c97a7a897f98bf801a01a3557c4fb20
Byron Wang <byronwang123919@gmail.com>
2023-05-15 02:18:13 -0700

    new context added for colour mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
 import { useState } from "react";
-import { EditContext } from "./helper/Context";
+import { EditContext, ColourContext } from "./helper/Context";
+import { useEffect } from "react";
 
 function App() {
   const [canEdit, setCanEdit] = useState(false);
+  const [colourMode, setColourMode] = useState(null);
+  useEffect(() => {
+    const savedColour = localStorage.getItem("colourMode");
+    if (savedColour === null || savedColour === "") {
+      localStorage.setItem("colourMode", "tangerine");
+    }
+  }, []);
 
   return (
-    <EditContext.Provider value={{ canEdit, setCanEdit }}>
-      <HomePage />
-    </EditContext.Provider>
+    <ColourContext.Provider value={{ colourMode, setColourMode }}>
+      <EditContext.Provider value={{ canEdit, setCanEdit }}>
+        <HomePage />
+      </EditContext.Provider>
+    </ColourContext.Provider>
   );
 }
 
--- src/helper/Context.js
+++ src/helper/Context.js
@@ -1,3 +1,4 @@
 import { createContext } from "react";
 
 export const EditContext = createContext({});
+export const ColourContext = createContext({});
